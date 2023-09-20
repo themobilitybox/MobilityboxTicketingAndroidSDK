e0bae17aa8e5060c15c62ef52e8ea5d19fd4ba22
tim.krusch <tim@vesputi.com>
2023-09-20 09:07:43 +0200

    fix parcelize of product and area coordinates
    add own serializer and deserializer for jsonelement attributes

--- mobilitybox-ticketing-android/src/main/java/com/vesputi/mobilitybox_ticketing_android/models/MobilityboxArea.kt
+++ mobilitybox-ticketing-android/src/main/java/com/vesputi/mobilitybox_ticketing_android/models/MobilityboxArea.kt
@@ -1,6 +1,8 @@
 package com.vesputi.mobilitybox_ticketing_android.models
 
+import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.Gson
 import com.google.gson.JsonElement
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
@@ -19,11 +21,40 @@ data class MobilityboxAreaProperties(
     var local_zone_name: String
 ) : Parcelable
 
-@Parcelize
+
 data class MobilityboxAreaGeometry(
     var type: String,
-    var coordinates: @RawValue JsonElement
-) : Parcelable
+    var coordinates: JsonElement
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        Gson().fromJson(parcel.readString(), JsonElement::class.java)
+    ) {
+    }
+
+    fun coordinatesToString(): (String){
+        return Gson().toJson(coordinates)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(type)
+        parcel.writeString(coordinatesToString())
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MobilityboxAreaGeometry> {
+        override fun createFromParcel(parcel: Parcel): MobilityboxAreaGeometry {
+            return MobilityboxAreaGeometry(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MobilityboxAreaGeometry?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
 @Parcelize
 data class MobilityboxTicketArea(
--- mobilitybox-ticketing-android/src/main/java/com/vesputi/mobilitybox_ticketing_android/models/MobilityboxProduct.kt
+++ mobilitybox-ticketing-android/src/main/java/com/vesputi/mobilitybox_ticketing_android/models/MobilityboxProduct.kt
@@ -1,11 +1,12 @@
 package com.vesputi.mobilitybox_ticketing_android.models
 
+import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.Gson
 import com.google.gson.JsonElement
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 
-@Parcelize
 data class MobilityboxProduct(
     var id: String,
     val recommended_successor_is: String?,
@@ -21,8 +22,27 @@ data class MobilityboxProduct(
     var validity_in_minutes: Int?,
     var area_id: String,
     var is_subscription: Boolean,
-    var identification_medium_schema: @RawValue JsonElement
+    var identification_medium_schema: JsonElement
 ): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readInt(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString().toString(),
+        parcel.readByte() != 0.toByte(),
+        Gson().fromJson(parcel.readString(), JsonElement::class.java)
+    ) {
+    }
+
     fun getTitle(): (String) {
         val customer_type_string = if (customer_type == "adult") " Erwachsener" else ( if (customer_type == "child") " Kind" else "")
         val ticket_type_string = if (ticket_type == "single") "Einzelticket" else ( if (ticket_type == "day") "Tagesticket" else "")
@@ -40,6 +60,42 @@ data class MobilityboxProduct(
                 }
             }
             else -> return ""
+        }
+    }
+
+    fun identificationMediumSchemaToString(): (String){
+        return Gson().toJson(identification_medium_schema)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(recommended_successor_is)
+        parcel.writeString(recommended_successor_of)
+        parcel.writeString(local_ticket_name)
+        parcel.writeString(local_validity_description)
+        parcel.writeString(ticket_type)
+        parcel.writeString(customer_type)
+        parcel.writeInt(price_in_cents)
+        parcel.writeString(currency)
+        parcel.writeString(duration_definition)
+        parcel.writeValue(duration_in_minutes)
+        parcel.writeValue(validity_in_minutes)
+        parcel.writeString(area_id)
+        parcel.writeByte(if (is_subscription) 1 else 0)
+        parcel.writeString(identificationMediumSchemaToString())
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MobilityboxProduct> {
+        override fun createFromParcel(parcel: Parcel): MobilityboxProduct {
+            return MobilityboxProduct(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MobilityboxProduct?> {
+            return arrayOfNulls(size)
         }
     }
 }
